79368c4a68e274970ab5cfd3e67ee5cc1bd48fd6
CodeWCBrandon <christian.brandon@binus.ac.id>
2025-05-25 20:50:03 +0700

    setting up the loader.s (initializing the stack pointer to the kernel, adding the magic_number for GRUB (bootloader), saving boot information from ax and bx register, halt the CPU

---
+++ loader.s
@@ -0,0 +1,28 @@
+.set MAGIC_NUMBER, 0x1badb002 ; magic number for GRUB
+.set FLAGS, (1 << 0 | 1 << 1)
+.set CHECKSUM, -(MAGIC_NUMBER + FLAGS)
+
+.section .multiboot
+    .long MAGIC_NUMBER
+    .long FLAGS
+    .long CHECKSUM
+
+.section .text
+    .global loader
+    .extern kernelMain
+
+
+loader:
+    mov $kernal_stack, %esp
+    push %eax
+    push %ebx
+    call kernelMain
+
+_hang:
+    cli
+    hlt
+    jmp _hang
+
+.section .bss
+    .space 2*1024*1024 ; 2MiB spacing for stack pointer
+    kernel_stack:
